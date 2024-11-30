16c4909cbc39ab7b61ce07ca2087b0873678aabc
Loukas <loukasbass@gmail.com>
2024-11-30 19:02:26 +0100

    "addind cars on project"

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="Section_top">
+        <div className="content">
+          <h1 className="text-8xl text-white">Project de fin de formation</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
